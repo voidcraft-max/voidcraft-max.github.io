1eb1337ae6c1349a2cb7e55ff097c47cce2361e4
Preston <PrestonT500WasTaken@proton.me>
2024-10-29 07:13:01 -0700

    Update ga4.js

--- js/ga4.js
+++ js/ga4.js
@@ -1,69 +1,37 @@
 function addGoogleAnalyticsTags() {
     var currentDomain = window.location.hostname;
+    var trackingId;
 
-    if (currentDomain === "eaglercraftx1-8.github.io") {
-        // GitHub Pages Analytics
-        var githubAnalyticsScript = document.createElement('script');
-        githubAnalyticsScript.async = true;
-        githubAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-C44R8LD9MC';
-        document.head.appendChild(githubAnalyticsScript);
-
-        var githubScriptContent = document.createElement('script');
-        githubScriptContent.innerHTML = `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-C44R8LD9MC');
-        `;
-        document.head.appendChild(githubScriptContent);
-
-    } else if (currentDomain === "eaglercraftx1-8.netlify.app") {
-        // Netlify Analytics
-        var netlifyAnalyticsScript = document.createElement('script');
-        netlifyAnalyticsScript.async = true;
-        netlifyAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-0R2ZXFSYVT';
-        document.head.appendChild(netlifyAnalyticsScript);
-
-        var netlifyScriptContent = document.createElement('script');
-        netlifyScriptContent.innerHTML = `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-0R2ZXFSYVT');
-        `;
-        document.head.appendChild(netlifyScriptContent);
-
-    } else if (currentDomain === "eaglercraftx1-8.onrender.com") {
-        // Render Analytics
-        var renderAnalyticsScript = document.createElement('script');
-        renderAnalyticsScript.async = true;
-        renderAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-MND1TVBSXV';
-        document.head.appendChild(renderAnalyticsScript);
-
-        var renderScriptContent = document.createElement('script');
-        renderScriptContent.innerHTML = `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-MND1TVBSXV');
-        `;
-        document.head.appendChild(renderScriptContent);
-
-    } else {
-        // Else (Fallback) Analytics
-        var fallbackAnalyticsScript = document.createElement('script');
-        fallbackAnalyticsScript.async = true;
-        fallbackAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-81F615LDEZ';
-        document.head.appendChild(fallbackAnalyticsScript);
-
-        var fallbackScriptContent = document.createElement('script');
-        fallbackScriptContent.innerHTML = `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-81F615LDEZ');
-        `;
-        document.head.appendChild(fallbackScriptContent);
+    switch (currentDomain) {
+        case "eaglercraftx1-8.github.io":
+            // GitHub Pages Analytics
+            trackingId = "G-C44R8LD9MC";
+            break;
+        case "eaglercraftx1-8.netlify.app":
+            // Netlify Analytics
+            trackingId = "G-0R2ZXFSYVT";
+            break;
+        case "eaglercraftx1-8.onrender.com":
+            // Render Analytics
+            trackingId = "G-MND1TVBSXV";
+            break;
+        case "client1.eaglercraft.win":
+            // GitHub Pages / Client 1
+            trackingId = "G-SGELV1H17J";
+            break;
+        default:
+            // Fallback Analytics
+            trackingId = "G-81F615LDEZ";
     }
-}
-addGoogleAnalyticsTags();
+
+    if (trackingId) {
+        var analyticsScript = document.createElement('script');
+        analyticsScript.async = true;
+        analyticsScript.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
+        document.head.appendChild(analyticsScript);
+
+        var scriptContent = document.createElement('script');
+        scriptContent.innerHTML = `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('
