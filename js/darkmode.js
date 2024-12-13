254b02db5eed13d21b1660944ce590dd4e39a005
clashofclanselixircollector <clashofclanselixircollector@noreply.localhost>
2024-12-13 14:30:02 -0800

    call me golf cause I'm in that hole

--- js/darkmode.js
+++ js/darkmode.js
@@ -71,7 +71,7 @@ $(document).ready(function() {
         }
     }
     if (localStorage.getItem('darkModeOn') == true) {
-        $('#darkModeCheckbox').prop("checked");
+        document. getElementById('darkModeCheckbox'). checked = true; 
         var checked = $("#darkModeCheckbox").is(":checked");
         updateDarkMode(checked);
     }
